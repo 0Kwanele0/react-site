96038d1bdcd96b7400fbaa0e4cd23c1736debe71
0Kwanele0 <kwanelegamedze4@gmail.com>
2021-07-29 13:38:04 +0200

    added animation

--- src/showcase.js
+++ src/showcase.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import './showcase.css'
 import img from './images/custmer-support-colored-.png'
 import Button from './components/button'
+import { TweenMax, Power3} from 'gsap'
+
 
 function Showcase() {
+
+    let headerItem = useRef()
+    let imageItem = useRef()
+    
+    useEffect(() => {
+        TweenMax.from(headerItem, 1, {
+            opacity: 0,
+            x: -10
+        })
+        TweenMax.from(imageItem, 1, {
+            opacity: 0,
+            x: 10
+        })
+    }, [])
+
     return (
         <div className="main">
-            <div className="left">
+            <div ref={el => {
+                headerItem = el
+            }} className="left">
                 <h1>Let us help you create and launch your product</h1>
                 <div className="btn-div">
                     <Button text="Contact Us" />
                     <Button text="Get in touch" />
                 </div>
             </div>
-            <div className="right">
+            <div ref={(el) => {
+                imageItem = el
+            }} className="right">
                 < img className='img' src={img} />
             </div>
         </div>
