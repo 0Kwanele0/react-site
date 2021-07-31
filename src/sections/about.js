9e0c9aa7b442cc2987fcbb51ee9773fc706e3046
0Kwanele0 <kwanelegamedze4@gmail.com>
2021-07-31 14:14:29 +0200

    added some cool anims

--- src/sections/about.js
+++ src/sections/about.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import Image3 from '../images/seo.png'
 import './styles/about.css'
+import { useIntersection } from 'react-use'
+import { gsap } from 'gsap'
+import { useRef, useEffect } from 'react'
+
+
+
 
 export const About = () => {
+
+    const about = useRef()
+
+    const observer = useIntersection(about, {
+        root: null,
+        margin: 0,
+        threshold: 0.2
+    })
+
+    function startAnim(element, ones){
+        gsap.to(element, 2, {
+            x: 0,
+            opacity: 1,
+            ease: "bounce"
+        })
+        gsap.to(ones, 2, {
+            x: 0,
+            opacity: 1,
+            ease: "bounce"
+        })
+    }
+    function endAnim(element, ones){
+        gsap.to(element, 2, {
+            x: -40,
+            opacity: 0,
+            ease: "bounce"
+        })
+        gsap.to(ones, 2, {
+            x: 40,
+            opacity: 0,
+            ease: "bounce"
+        })
+    }
+
+    observer && observer.intersectionRatio > 0.2 ? startAnim(".about-left", ".about-right") : endAnim(".about-left", ".about-right");
+
+
     return (
-        <div className="about-container">
+        <div ref={ about } className="about-container">
             <div className="about-left">
                 <img className="imagee" img src={Image3} />
             </div>
